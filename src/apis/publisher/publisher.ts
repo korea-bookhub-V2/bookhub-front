34bba09ff4c6f364c0ae517358b7551e735b5939
choiseoyoon <chsu0904@naver.com>
2025-07-21 11:59:11 +0900

    feat : publisher- csy

--- src/apis/publisher/publisher.ts
+++ src/apis/publisher/publisher.ts
@@ -1,6 +1,6 @@
 import { ResponseDto } from "@/dtos"
 import { PublisherListResponseDto, PublisherResponseDto } from "@/dtos/publishers/publisher.response.dto"
-import { DELETE_PUBLISHER_URL, GET_ALL_PUBLISHER_URL, POST_PUBLISHER_URL, PUT_PUBLISHER_URL } from "./publisherUrl"
+import { DELETE_PUBLISHER_URL, GET_ALL_PUBLISHER_URL, GET_PUBLISHER_URL, POST_PUBLISHER_URL, PUT_PUBLISHER_URL } from "./publisherUrl"
 import { axiosInstance, bearerAuthorization, responseErrorHandler, responseSuccessHandler } from "../axiosConfig"
 import { AxiosError } from "axios";
 import { PublisherRequestDto } from "@/dtos/publishers/publisher.request.dto";
@@ -84,4 +84,24 @@ PageResponseDto<PublisherResponseDto>| PublisherResponseDto[]>> => {
         error as AxiosError<ResponseDto<PageResponseDto<PublisherResponseDto>| PublisherResponseDto[]>>
     );
  }
+}
+
+export const getPublisher = async(
+    
+    publisherId : number,
+    accessToken : string
+): Promise<
+ResponseDto<PublisherResponseDto>> => {
+    try{
+        const response = await axiosInstance.get(
+            GET_PUBLISHER_URL(publisherId),
+            bearerAuthorization(accessToken)
+        );
+        return responseSuccessHandler(response);
+
+    }catch(error){
+        return responseErrorHandler(
+            error as AxiosError<ResponseDto<PublisherResponseDto>>
+        )
+    }
 }
